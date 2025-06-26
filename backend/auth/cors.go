61b338e12c8e16119a6c4dd7131dc00cb3577b81
omkhalid <omark.janahi@gmail.com>
2025-06-26 15:54:24 +0300

    netlify it

--- backend/auth/cors.go
+++ backend/auth/cors.go
@@ -4,16 +4,21 @@ import "net/http"
 
 func WithCORS(handler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://teamqraphql.netlify.app")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST,PUT, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		// Allow your production origin *and* localhost for local dev
+		origin := r.Header.Get("Origin")
+		if origin == "https://teamqraphql.netlify.app" || origin == "http://localhost:8888" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS")
+		// Add Authorization here!
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
-
 		handler(w, r)
 	}
 }
